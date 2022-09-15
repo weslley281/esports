2b0d24143f2e3b1a82e0b20f378046831dc51962
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-15 06:17:06 -0400

    a

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './styles/main.css';
+import logoImg from './assets/Logo.svg';
 
 export function App() {
-  return <div className="w-8 h-8 bg-black text-zinc-400"></div>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center m-20">
+      <img width={300} src={logoImg} alt="logo do site" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          Duo
+        </span>{' '}
+        está aqui.
+      </h1>
+    </div>
+  );
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/Fundo.svg')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
       },
     },
   },
