6bb11b60d7ede91318dbbd2a5e8816d9f48b38ee
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-09-15 10:31:46 -0400

    a

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,ts,tsx}', './index.html'],
+  content: ['./src/**/*.tsx', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+
     extend: {
       backgroundImage: {
-        galaxy: "url('/Fundo.svg')",
+        galaxy: "url('/background-galaxy.png')",
         'nlw-gradient':
           'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
         'game-gradient':
